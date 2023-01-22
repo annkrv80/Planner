4d4e33fa2c53bdd536c188d69a84501a2748001b
Anna <annkrv80@yandex.ru>
2023-01-22 23:46:12 +0700

    Main

--- Main.java
+++ Main.java
@@ -1,7 +1,3 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -11,53 +7,31 @@ public class Main {
 
     public static void main(String[] args) {
         List<Task> listTask = new ArrayList<>();
-        System.out.println("Хочешь записать задачу, введи 'yes'");
-        Scanner in = new Scanner(System.in);
-        String start = "yes";
-        start = in.next();
-        while (start.equals("yes")) {
-            Task task = new Task(null, 0);
-            task.enterTask();
-            listTask.add(task);
-            System.out.println("Ввести еще одну?");
-            start = in.next();
-        }
+        Task task = TaskBuilder.getInstance().priority(3).dateStart(02022023f)
+                .descriptionTask("zaplatit nalogi").dateExpiration(10022023f).build();
+        Task task2 = TaskBuilder.getInstance().priority(2).dateStart(100032023f).timeStart(1200f)
+                .descriptionTask("manikyur").contactPerson("Lena").contactNumber(89515701333l).build();
+        Task task3 = TaskBuilder.getInstance().descriptionTask("pozvonit mame").build();
+        listTask.add(task);
+        listTask.add(task2);
+        listTask.add(task3);
         Collections.sort(listTask, new SortPriority());
+
+        String start;
+        Scanner in = new Scanner(System.in);
         System.out.println("Записать данные в таблицу, введи '1', Вывести данные на консоль, введи ' 2'");
-        System.out.println("Закончить, введи '3'");
         start = in.next();
         if (start.equals("1")) {
-            try (FileWriter writer = new FileWriter("plan.cvs")) {
-                int size = listTask.size();
-                for (int i = 0; i < size; i++) {
-                    String str = listTask.get(i).toString();
-                    writer.write(str);
-                    if (i < size - 1)
-                        writer.write("\n");
-                }
-                writer.close();
-            } catch (IOException e) {
+            WritingToFile wr = new WritingToFile();
+            wr.writingToFile(listTask);
 
-                e.printStackTrace();
-            }
         } else if (start.equals("2")) {
-            try (BufferedReader csvReader = new BufferedReader(new FileReader("plan.cvs"))) {
-                String row;
-                while ((row = csvReader.readLine()) != null) {
-                    String[] data = row.split(",");
-                    for (int i = 0; i < data.length; i++) {
-                        System.out.println(data[i]);
-                    }
-                }
-                csvReader.close();
-            } catch (IOException e) {
+            OutputToConsole op = new OutputToConsole();
+            op.outputToConcole();
 
-                e.printStackTrace();
-            }
-        } else if (start.equals("3")) {
-            System.out.println("Хорощего дня");
         } else {
             System.out.println("Ошибка выбора");
         }
     }
+
 }
